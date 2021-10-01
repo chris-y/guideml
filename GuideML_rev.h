ea2a6463ada817115ca222333bd9be290d8d0e7e
Chris Young <chris@unsatisfactorysoftware.co.uk>
2021-10-01 18:10:31 +0100

    Bump version to 3.16, update docs

--- GuideML_rev.h
+++ GuideML_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		3
-#define REVISION	15
-#define DATE		"21.6.2008"
-#define VERS		"GuideML 3.15"
-#define VSTRING		"GuideML 3.15 (21.6.2008)\r\n"
-#define VERSTAG		"\0$VER: GuideML 3.15 (21.6.2008)"
+#define REVISION	16
+#define DATE		"1.10.2021"
+#define VERS		"GuideML 3.16"
+#define VSTRING		"GuideML 3.16 (1.10.2021)\r\n"
+#define VERSTAG		"\0$VER: GuideML 3.16 (1.10.2021)"
--- GuideML_rev.s
+++ GuideML_rev.s
@@ -1,21 +1,21 @@
 VERSION = 3
-REVISION = 15
+REVISION = 16
 
 .macro DATE
-.ascii "21.6.2008"
+.ascii "1.10.2021"
 .endm
 
 .macro VERS
-.ascii "GuideML 3.15"
+.ascii "GuideML 3.16"
 .endm
 
 .macro VSTRING
-.ascii "GuideML 3.15 (21.6.2008)"
+.ascii "GuideML 3.16 (1.10.2021)"
 .byte 13,10,0
 .endm
 
 .macro VERSTAG
 .byte 0
-.ascii "$VER: GuideML 3.15 (21.6.2008)"
+.ascii "$VER: GuideML 3.16 (1.10.2021)"
 .byte 0
 .endm
