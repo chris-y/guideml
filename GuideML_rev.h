397064b43eebdb074fed5783f8f18fae32cf38ee
Chris Young <chris@unsatisfactorysoftware.co.uk>
2023-01-04 11:01:21 +0000

    Tweak dark mode so background isn't 100% black Update date

--- GuideML_rev.h
+++ GuideML_rev.h
@@ -1,6 +1,6 @@
 #define VERSION		3
 #define REVISION	17
-#define DATE		"30.12.2022"
+#define DATE		"4.1.2023"
 #define VERS		"GuideML 3.17"
-#define VSTRING		"GuideML 3.17 (30.12.2022)\r\n"
-#define VERSTAG		"\0$VER: GuideML 3.17 (30.12.2022)"
+#define VSTRING		"GuideML 3.17 (4.1.2023)\r\n"
+#define VERSTAG		"\0$VER: GuideML 3.17 (4.1.2023)"
